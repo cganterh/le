866590b61e48d7c459ceeb52693db15d7fadab4f
Cristóbal Ganter <cganterh@gmail.com>
2017-06-10 23:35:17 -0400

    Moved package configuration from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,47 +1,5 @@
 """Setup script for Le."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-from codecs import open
-from os import path
-
-current_directory = path.dirname(__file__)
-absolute_directory = path.abspath(current_directory)
-readme_path = path.join(absolute_directory, 'README.md')
-
-with open(readme_path, encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='le',
-    version='1.0.0',
-    description='A silly chatbot for telegram.',
-    long_description=long_description,
-    url='https://github.com/cganterh/le',
-    author='Cristóbal Ganter',
-    author_email='cganterh@gmail.com',
-    license='MIT',
-
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: Spanish',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Communications :: Chat',
-    ],
-
-    keywords='python python3 telegram-bot chatbot',
-    packages=find_packages(),
-
-    install_requires=[
-        'python-telegram-bot ~= 6.0',
-    ],
-
-    entry_points={
-        'console_scripts': [
-            'le=le.run:run',
-        ],
-    },
-)
+setup()
